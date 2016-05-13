f11d9f828f1768833532c5403965f68e2b9128b3
medi / Giso Grimm <g.grimm@uni-oldenburg.de>
2016-05-13 13:38:26 +0200

    fixed universe and data format

--- src/test_artnetdmx.cc
+++ src/test_artnetdmx.cc
@@ -4,9 +4,10 @@ int main(int argc, char** argv)
 {
   artnetDMX_t dmx("192.168.0.9","6454");
   std::vector<uint16_t> data;
-  data.resize(1);
-  data[0] = 0x01ff;
-  dmx.send(1,data);
+  data.resize(32);
+  for(uint32_t k=0;k<32;k++)
+    data[k] = 0x01ff;
+  dmx.send(0,data);
 }
 
 /*
